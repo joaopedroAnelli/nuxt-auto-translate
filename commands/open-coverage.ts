ef29f617b9c41faa69821bdd8cba7398a8f53d69
joaopedroAnelli <joaopedroanelli99@gmail.com>
2022-09-22 21:46:47 -0300

    Coverage fix

--- commands/open-coverage.ts
+++ commands/open-coverage.ts
@@ -1,7 +1,13 @@
-import { resolve } from 'path';
+import { resolve, dirname } from 'path';
 import { exec } from 'child_process';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
 
 const url = resolve(__dirname, '../coverage/lcov-report/index.html');
+
 const start =
   process.platform === 'darwin'
     ? 'open'
