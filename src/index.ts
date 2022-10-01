4a147909bd3136d6691064caa255c7a84cceb5d5
joaopedroAnelli <joaopedroanelli99@gmail.com>
2022-10-01 13:19:14 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { resolve } from 'node:path';
 import { Module } from '@nuxt/types';
 import { BaseOptions as I18nOptions } from '@nuxtjs/i18n';
 import TranslationsBuilder from './services/TranslationsBuilder';
-import nuxtAutoTranslateRestApi from '~/server/nuxtAutoTranslateRestApi';
+import ServerInitializer from '~/server/ServerInitializer';
+import NuxtAutoTranslateServerApp from '~/server/NuxtAutoTranslateExpress';
 
 export interface ModuleOptions {
   i18n: I18nOptions;
@@ -32,9 +33,13 @@ const translateMessagesModule: Module<ModuleOptions> = function (options) {
 
   this.addPlugin(resolve(__dirname, 'plugins/registerComponent.ts'));
 
+  const app = new ServerInitializer();
+
+  app.init();
+
   this.addServerMiddleware({
     path: '/nuxt-auto-translate',
-    handler: nuxtAutoTranslateRestApi,
+    handler: NuxtAutoTranslateServerApp.getInstance(),
   });
 };
 
