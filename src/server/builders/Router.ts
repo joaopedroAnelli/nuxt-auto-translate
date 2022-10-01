d4adfbd4c27611357acfddfac4debab9b72d8f17
joaopedroAnelli <joaopedroanelli99@gmail.com>
2022-10-01 13:18:41 -0300

    Create Router.ts

---
+++ src/server/builders/Router.ts
@@ -0,0 +1,21 @@
+import ModuleRouter from '~/server/contracts/ModuleRouter';
+import ServerBuilder from '~/server/contracts/ServerBuilder';
+
+export default class Router implements ServerBuilder {
+  routers: ModuleRouter[];
+  next: ServerBuilder | undefined;
+
+  constructor(routers: ModuleRouter[]) {
+    this.routers = routers;
+  }
+
+  build() {
+    this.routers.forEach((router) => router.route());
+
+    if (this.next) this.next.build();
+  }
+
+  setNext(next: ServerBuilder): void {
+    this.next = next;
+  }
+}
