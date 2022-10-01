83cc7a00d523a59c65ddf37b768921f5be950945
joaopedroAnelli <joaopedroanelli99@gmail.com>
2022-10-01 13:19:52 -0300

    Create Router.ts

---
+++ src/server/modules/messages/Router.ts
@@ -0,0 +1,20 @@
+import { Express, Router } from 'express';
+import ModuleRouter from '~/server/contracts/ModuleRouter';
+import Handler from '~/server/modules/messages/Handler';
+
+export default class MessageRouter implements ModuleRouter {
+  handler: Handler;
+  app: Express;
+  router: Router = Router();
+
+  constructor(app: Express, messageHandler: Handler) {
+    this.app = app;
+    this.handler = messageHandler;
+  }
+
+  route() {
+    this.router.post('', this.handler.createMessage);
+
+    this.app.use('/messages', this.router);
+  }
+}
